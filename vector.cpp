280ee51bb4d1cc6c5ef2f3e202df820585f8a06f
rao-shab <raoshabmanish28@gmail.com>
2026-05-10 11:10:37 +0530

    Add vector example with output to console

---
+++ vector.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+vector<int> v = {1,2,3,4};
+
+for (int i = 0; i<v.size() ,i++ ){
+    cout << v[i]<<endl;
+}
+
+
+return 0;
+
+}
