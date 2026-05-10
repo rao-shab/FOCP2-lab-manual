d797a32c7c4a2fda4571b7a59416738ae60995bb
rao-shab <raoshabmanish28@gmail.com>
2026-05-10 10:58:51 +0530

    Add trav class with array handling method

---
+++ arr_tac.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class trav{
+
+private:
+int *arr;
+int size;
+
+public:
+
+
+
+
+
+void arrayhandle(int s){
+size = s;
+int arr = arr[s];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
