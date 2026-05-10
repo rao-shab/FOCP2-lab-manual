36b4c42d63f1d85e8f1f077ff88b96e3013b2f18
rao-shab <raoshabmanish28@gmail.com>
2026-05-10 10:57:09 +0530

    Add program to count numbers divisible by 3 and 5

---
+++ q31.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[5];
+    int count = 0;
+
+    cout << "Enter 5 elements:\n";
+    for (int i = 0; i < 5; i++) {
+        cin >> a[i];
+        if (a[i] % 3 == 0 && a[i] % 5 == 0) {
+            count++;
+        }
+    }
+
+    cout << "Numbers divisible by both 3 and 5 = " << count;
+    return 0;
+}
