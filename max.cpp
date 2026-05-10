815ad67e587e6eb7c53c82d4af52cbb7d67daa74
rao-shab <raoshabmanish28@gmail.com>
2026-05-10 11:06:21 +0530

    Implement max function to find maximum value

---
+++ max.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+int a = 10, b = 20;
+int c = 30;
+int e = 40;
+int d = max({a, b, c,e});
+cout<<"the maximum value is "<<d;
+
+
+
+
+}
